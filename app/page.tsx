43aaebe9420004f9855b5cb1f283746af759f8d3
yousef <yzabalawy@gmail.com>
2024-02-02 16:10:07 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/main/Hero";
+import Skills from "@/components/main/Skills";
+import Image from "next/image";
+import Encryption from './../components/main/Encryption';
+import Projects from "@/components/main/Projects";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex  flex-col gap-20">
+        <Hero />
+        <Skills/>
+        <Encryption/>
+        <Projects/>
+
+      </div>
+    </main>
+  );
+}
